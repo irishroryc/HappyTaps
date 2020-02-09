352687a8b509ba8516fe843a5f25893e50b072b9
irishroryc <connolly.rory@gmail.com>
2020-02-09 13:34:01 -0500

    Separating out services

---
+++ web/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+# App Engine looks for an an app called 'app' in main.py
+# Can override with 'entrypoint' in app.yaml if desired
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index.html')
+def welcome():
+    return "Welcome!"
