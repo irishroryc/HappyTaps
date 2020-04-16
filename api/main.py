1871258cb82f5b880ef4fe63e22f7b03014ebaaa
irishroryc <connolly.rory@gmail.com>
2020-04-15 22:10:20 -0400

    Restoring google cloud debugger and resolving naming inconsistencies in secret manager calls

--- api/main.py
+++ api/main.py
@@ -23,7 +23,7 @@ YELP_URL = "https://api.yelp.com/v3/businesses/search"
 #YELP_HEADERS = {'Authorization':'Bearer '+YELP_API_KEY}
 
 # Using Google secrets manager to populate YELP_API_KEY
-secret_client = secretmanager.SecretManagerServiceClient()
+secret_client = secretmanager_v1.SecretManagerServiceClient()
 secret_path = secret_client.secret_version_path('clear-router-191420','YELP_API_KEY',1)
 YELP_API_KEY = secret_client.access_secret_version(secret_path).payload.data.decode("utf-8")
 YELP_HEADERS = {'Authorization':'Bearer '+YELP_API_KEY}
